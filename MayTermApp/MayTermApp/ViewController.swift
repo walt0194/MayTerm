2dc806c4cf6296baaae8fa8aba82a32f60628596
walt0194 <waltb18082@isd273.org>
2018-05-31 12:23:11 -0500

    Worked on App

--- MayTermApp/MayTermApp/ViewController.swift
+++ MayTermApp/MayTermApp/ViewController.swift
@@ -7,19 +7,44 @@
 //
 
 import UIKit
+import SwiftSocket
 
 class ViewController: UIViewController {
+    
+    let client = TCPClient(address: "73.94.99.15", port: 6394)
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var field: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        field.delegate = self
+        
+        switch client.connect(timeout: 10) {
+        case .success:
+            label.text = "connected"
+        case .failure(let error):
+            label.text = String(describing: error)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func btnPress(_ sender: Any) {
+        client.send(string: field.text!)
+        label.text = field.text
+    }
+}
 
-
+extension ViewController: UITextFieldDelegate{
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
